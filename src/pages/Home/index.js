23aecf5c2c34d9a79d09866da7cb38d0f0bd4657
JohnLennoN <jljohnlennon@hotmail.com>
2020-06-26 15:11:05 -0300

    Criação de menu

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,37 @@
-import React, {useContext} from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { Background, Container, Nome, Saldo, Title, List } from './styles';
 
-import  { AuthContext } from '../../contexts/auth'
+import { AuthContext } from '../../contexts/auth';
+
+import Header from '../../components/Header';
+
+import HistoricoList from '../../components/HistoricoList';
+
 
 export default function Home() {
-    const { user, signOut } = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
+
+    const [historico, setHistorico] = useState([
+        { key: '1', tipo: 'receita', valor: 9000.000 },
+        { key: '2', tipo: 'receita', valor: 2500.000 },
+        { key: '3', tipo: 'despesa', valor: 12000 },
+        { key: '4', tipo: 'receita', valor: 200.000 },
+    ]);
 
     return (
-        <View>
-            <Text>{user && user.nome}</Text>
-            <Text>{user && user.email}</Text>
-            <Button
-            title='Sair'
-            onPress={() => signOut()}/>
-        </View>
+        <Background>
+            <Header />
+            <Container>
+                <Nome>John</Nome>
+                <Saldo>R$ 100.000.000,00</Saldo>
+            </Container>
+            <Title>Ultimas movimentações</Title>
+
+            <List
+                showVerticalScrollIndicator={false}
+                data={historico}
+                KeyExtrator={item => item.key}
+                renderItem={({ item }) => (<HistoricoList />)} />
+        </Background>
     );
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/auth';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import Header from '../../components/Header';
 
 import {Container, Nome, NewLink, NewText, Logout, LogoutText} from './styles'
 
@@ -10,6 +11,7 @@ export default function Profile() {
     const { user, signOut } = useContext(AuthContext);
     return (
         <Container>
+            <Header/>
             <Nome>
                 {user && user.nome}
             </Nome>
